f903a07cdfea6d5199020974176ac2e4a4afd5f5
Kacper Lutyński <lut3k777@gmail.com>
2024-06-24 20:12:59 +0200

    chore: Update ESLint rules and add new colors to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
+import colors from 'tailwindcss/colors';
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.indigo,
+        secondary: colors.blue,
+        neutral: colors.gray,
+        success: colors.green,
+        danger: colors.red,
+        warning: colors.yellow,
+      },
+    },
+    theme: {},
   },
   plugins: [],
 };
